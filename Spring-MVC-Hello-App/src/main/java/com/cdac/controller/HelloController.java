28f0d1ba819c17f46f3ee748c4173df20a289365
DNYANESHWAR DAPKAR <100856491+dnyaneshward1997@users.noreply.github.com>
2023-04-19 09:38:41 +0530

    Add files via upload

---
+++ Spring-MVC-Hello-App/src/main/java/com/cdac/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.cdac.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = {"/hello.htm"})
+	public String sayHello(ModelMap model ) {
+		
+		model.put("msg", "Hello Everyone, Welcome to my Spring Project");
+
+		return "info";
+
+	}
+
+}
